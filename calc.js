ddcc3f42d4588625f5a2f403b3ea00a086e67c95
WillSpark11 <William.spark@outlook.com>
2021-07-02 14:09:57 -0500

    initialcommit

---
+++ calc.js
@@ -0,0 +1,45 @@
+function addition(x, y){
+    return x + y;
+}
+
+function subtraction(x, y){
+    return x - y;
+}
+
+function multiplication(x, y){
+    return x * y;
+}
+
+function division(x, y){
+    return x / y;
+}
+
+
+
+
+var a = addition(1,2);
+var b = multiplication(a,4);
+var c = subtraction(10, b);
+var d = division(c, 2);
+
+console.log(d);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ logic.js
@@ -0,0 +1,5 @@
+function changeBackground() {
+    document.getElementsByTagName("body")[0].style.backgroundColor = "blue";
+}
+
+window.onload = () => {changeBackground();}
